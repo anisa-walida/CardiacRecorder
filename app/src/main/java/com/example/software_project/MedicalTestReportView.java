587be235e2e3c464d577fbaefa0af44f48cc4514
anisa_walida <anisawalida206@gmail.com>
2023-07-01 01:48:40 +0600

    Anisa Update and delete(2)

--- app/src/main/java/com/example/software_project/MedicalTestReportView.java
+++ app/src/main/java/com/example/software_project/MedicalTestReportView.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.EditText;
@@ -26,6 +27,17 @@ public class MedicalTestReportView extends AppCompatActivity {
 
     FirebaseAuth mAuth = FirebaseAuth.getInstance();
     ArrayList<MedicalReportModel> list = new ArrayList<>();
+
+    //My edit
+    ArrayList<String> reportKeys = new ArrayList<>();
+    //My edit*/
+
+
+
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,24 +51,87 @@ public class MedicalTestReportView extends AppCompatActivity {
             recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
             adapter = new PMedicalReportAdapter(this, list);
+
+            //My edit
+            adapter.setReportKeys(reportKeys);
+
+            adapter.setEditButtonClickListener(new PMedicalReportAdapter.EditButtonClickListener() {
+                @Override
+                public void onEditButtonClick(String key) {
+                    // Handle the edit button click here
+                    // You now have access to the key of the database record to update it
+                    // Perform any actions you need based on the clicked edit button
+                    Intent intent = new Intent(MedicalTestReportView.this, Update_Medical_report.class);
+                    intent.putExtra("recordKey", key);
+                    startActivity(intent);
+                }
+            });
+
+            //My edit*/
+
+            //My delete
+            adapter.setDeleteButtonClickListener(new PMedicalReportAdapter.DeleteButtonClickListener() {
+                @Override
+                public void onDeleteButtonClick(int position) {
+                    // Handle the delete button click event for the item at the given position
+                    // Perform the deletion logic here, e.g., remove the item from the list and database
+                    String reportKey = reportKeys.get(position);
+
+                    // Remove the item from the database
+                    database.child(reportKey).removeValue();
+
+                    // Remove the item from the list
+                    list.remove(position);
+
+                    // Notify the adapter that the item has been removed
+                    adapter.notifyItemRemoved(position);
+                }
+            });
+
+
+            //My delete
+
+
+
+
+
             recyclerView.setAdapter(adapter);
             database.addValueEventListener(new ValueEventListener() {
                 @Override
                 public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                    //My delete
+                    list.clear(); // Clear the list before adding new items
+                    reportKeys.clear(); // Clear the reportKeys before adding new keys
+                    //My delete
+
+
                     for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
                         MedicalReportModel medReport = dataSnapshot.getValue(MedicalReportModel.class);
                         list.add(medReport);
+                        //My edit
+                        reportKeys.add(dataSnapshot.getKey()); // Add the key to reportKeys
+                        //My edit*/
                     }
                     adapter.notifyDataSetChanged();
                 }
+
+
+
+
 
                 @Override
                 public void onCancelled(@NonNull DatabaseError error) {
 
                 }
+
+
+
             });
         }catch (Exception e) {
             Log.e("ERROR__", e.getClass().getName() + ":" + e.getMessage());
         }
     }
+
+
 }
